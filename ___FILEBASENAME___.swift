2e2f186497812d643e1bc98b4630764e6b9108f5
timsheu <timsheu.ash@gmail.com>
2017-04-06 10:41:45 +0800

    rename sharedInstance to shared

--- ___FILEBASENAME___.swift
+++ ___FILEBASENAME___.swift
@@ -14,7 +14,7 @@ protocol ___FILEBASENAMEASIDENTIFIER___Delegate {
 
 public class ___FILEBASENAMEASIDENTIFIER___: NSObject{
     let TAG = "___FILEBASENAMEASIDENTIFIER___"
-    static let sharedInstance: ___FILEBASENAMEASIDENTIFIER___ = {___FILEBASENAMEASIDENTIFIER___()}() // lazy initialization closure
+    static let shared: ___FILEBASENAMEASIDENTIFIER___ = {___FILEBASENAMEASIDENTIFIER___()}() // lazy initialization closure
     
     //MARK: initialization
     fileprivate override init(){
